4691b309d2cc43edfd51f78de278343469f8030f
yesthis1please <yesthis1please@gmail.com>
2022-06-15 23:08:18 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="App logo" />
+      </header>
+      Hello
+    </div>
+  );
+}
+
+export default App;
